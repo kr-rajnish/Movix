af07ba8b00e276926626c784168a21bf16049cf5
Rajnish Kumar <110758110+kr-rajnish@users.noreply.github.com>
2023-12-07 03:23:18 +0530

    HeroBanner commit

---
+++ src/pages/404/PageNotFound.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.scss";
+
+const PageNotFound = () => {
+  return <div>Page Not Found</div>;
+};
+
+export default PageNotFound;
---
+++ src/pages/searchResult/SearchResult.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.scss";
+
+const SearchResult = () => {
+  return <div>Search Result</div>;
+};
+
+export default SearchResult;
